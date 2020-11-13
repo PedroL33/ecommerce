2c90176806c939ed03416f38e3804ad5e5229467
peter <leepeter019@gmail.com>
2020-11-12 22:21:29 -0800

    Changed mongoose connection variables.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 require('dotenv').config()
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.mongoURI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
